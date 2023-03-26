3e27edd1c29b197a098080bbcf3ee70a0a98b731
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-03-26 15:19:26 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -1,20 +1,6 @@
 #include "main.h"
 #include <unistd.h>
 
-
-/**
- * _putchar - writes the character c to the stdout
- * @c: The character to print
- *
- * Return: On succes 1
- * On error, -1 is returned, and errno is et apporiately
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 /**
  * _printf - Emulate the original.
  *
---
+++ _putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to the stdout
+ * @c: The character to print
+ *
+ * Return: On succes 1
+ * On error, -1 is returned, and errno is et apporiately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
